66afe2411a79e8e52056652e7334cdb4a2875ab5
ShuoLiu98 <shuoliu1998@gmail.com>
2020-10-22 09:54:50 -0400

    s

--- chess/chess.js
+++ chess/chess.js
@@ -6,16 +6,3 @@ else {
   }
 }
 }
-
-function print() {
-
-  for(let i = 1; i< 8; i++) {
-    if (i % 2 ==0) {
-      console.log(' # # # #')
-    } else {
-      console.log('# # # #')
-    }
-  }
-}
-
-print()
