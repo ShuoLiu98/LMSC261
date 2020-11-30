a84489015a05599bcec1c5e4e084cee6cbdc1e7e
ShuoLiu98 <shuoliu1998@gmail.com>
2020-11-30 01:21:42 -0500

    1

---
+++ Python assignment/phthon four square.py
@@ -0,0 +1,15 @@
+print('+ - - - - + - - - - +')
+# first step
+count = 0
+while (count < 4):
+    count = count + 1
+    print('|         |         |')
+    # second step
+print('+ - - - - + - - - - +')
+count = 0
+while (count < 4):
+    count = count + 1
+    print('|         |         |')
+    # third step
+print('+ - - - - + - - - - +')
+# last step
